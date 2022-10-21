de57d5ee07c1b27fa50498e416cac6bceabb0aa0
DeukYu <ldy8070@naver.com>
2022-10-22 02:14:06 +0900

    SendBuffer
    SendBuffer

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -1,16 +1,31 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using ServerCore;
 
 namespace Server
 {
+    class Knight
+    {
+        public int hp;
+        public int attack;
+    }
     class GameSession : Session
     {
         public override void OnConnected(EndPoint endPoint)
         {
             Console.WriteLine($"OnConnected : {endPoint}");
-            byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to Server !");
+
+            Knight knight = new Knight() { hp = 100, attack = 10 };
+
+            ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
+            byte[] buffer = BitConverter.GetBytes(knight.hp);
+            byte[] buffer2 = BitConverter.GetBytes(knight.attack);
+            Array.Copy(buffer, 0, openSegment.Array, openSegment.Offset, buffer.Length);
+            Array.Copy(buffer2, 0, openSegment.Array, openSegment.Offset + buffer.Length, buffer2.Length);
+            ArraySegment<byte> sendBuff = SendBufferHelper.Close(buffer.Length + buffer2.Length);
+
             Send(sendBuff);
             Thread.Sleep(1000);
             Disconnect();
